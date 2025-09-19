9298647dac3dbf1e2b6150d4d9e215bc2f1f3b00
LouiseAdel <Louise.adel.jensen@rsyd.dk>
2025-09-19 10:51:56 +0200

    Update PCRBias_Statistics.R

--- PCRBias_Statistics.R
+++ PCRBias_Statistics.R
@@ -1,7 +1,8 @@
 #Commands for Calculation of Fisher's Exact Test and Log2 Fold-Change - Isoform Rediscovery, PCR Bias
 
-#Comparison of Δ10q levels in samples (S3 and S8) and controls (Sample 15 and 19) 
-#from Protocols (P) to levels detected by Direct RNA-seq (P7)
+#Comparison of Δ10q levels in controls (Sample 15 and 19) 
+#from Protocols (P) to levels detected by Direct RNA-seq (ONT-Direct)
+#For Short-read analysis, the Full-length reads are the average of junctions supporting the exon 9-10 and 10-11 connection without Δ10q.
 
 #Libraries ----
 library(readxl)
@@ -13,26 +14,28 @@ library(purrr)
 #Individual Protocols ----
 #Read data file
 data_controls <- read_excel("PCRBias_delta10q_Levels_controlsonly.xlsx")
-
 print(data_controls)
-# A tibble: 16 × 5
-#   SampleID    Protocol Delta10q_Percentage Delta10q_Reads FullLength_Reads
-# 1 Control_S15        1                4.43           2598            58646
-# 2 Control_S19        1               22.9            3328            14558
-# 3 Control_S15        2               17.0              10               59
-# 4 Control_S19        2                8.51              4               47
-# 5 Control_S15        3               21.3              36              169
-# 6 Control_S19        3               16.7              34              204
-# 7 Control_S15        4               44.5           17850            40139
-# 8 Control_S19        4               43.8           11671            26670
-# 9 Control_S15        5               44.0            1464             3331
-#10 Control_S19        5               46.7            2259             4834
-#11 Control_S15        6               13.5             577             4274
-#12 Control_S19        6               12.2            2344            19213
-#13 Control_S15        7                5                 8              160
-#14 Control_S19        7               12.2              14              115
-#15 Control_S15        8                1.18              3              254
-#16 Control_S19        8                1.13              5              442
+
+#SampleID    Protocol Delta10q_Percentage Delta10q_Reads FullLength_Reads
+#Control_S15        1                4.43           2598          58646  
+#Control_S19        1               22.9            3328          14558  
+#Control_S15        2               17.0              10             59  
+#Control_S19        2                8.51              4             47  
+#Control_S15        3               21.3              36            169  
+#Control_S19        3               16.7              34            204  
+#Control_S15        4               44.5           17850          40139  
+#Control_S19        4               43.8           11671          26670  
+#Control_S15        5               44.0            1464           3331  
+#Control_S19        5               46.7            2259           4834  
+#Control_S15        6               13.5             577           4274  
+#Control_S19        6               12.2            2344          19213  
+#Control_S15        7                5                 8            160  
+#Control_S19        7               12.2              14            115  
+#Control_S15        8                8.22              3             36.5
+#Control_S19        8                7.41              5             67.5
+
+#Protocol 1 is PacBio-WT, 2 ONT-WT-A, 3 ONT-WT-B, 4 ONT-Amplicon-1, 5 ONT-Amplicon-2, 6 ONT-Capture, 7 ONT-Direct, and 8 Ill-SR
+
 
 #Combine control counts
 combined_counts <- data_controls %>%
@@ -83,14 +86,13 @@ print(results_individual)
 
 #Result - Individual Protocols:
 #Protocol Delta10q_Reads FullLength_Reads Delta10q_Percentage  Log2FC Fisher_p
-#1           5926            73204                7.49  0.0158 1   e+ 0
-#2             14              106               11.7   0.655  1.79e- 1
-#3             70              373               15.8   1.09   6.13e- 4
-#4          29521            66809               30.6   2.05   1.89e-22
-#5           3723             8165               31.3   2.08   6.61e-23
-#6           2921            23487               11.1   0.578  4.97e- 2
-#8              8              696                1.14 -2.70   6.68e- 7
-
+#Ill-SR                      8              104                7.14 -0.0525 1e+0          NS
+#ONT-Amplicon-1          29521            66809               30.6   2.05   1.89e-22      S
+#ONT-Amplicon-2           3723             8165               31.3   2.08   6.61e-23      S
+#ONT-Capture              2921            23487               11.1   0.578  4.97e-2       S
+#ONT-WT-A                   14              106               11.7   0.655  1.79e-1       NS
+#ONT-WT-B                   70              373               15.8   1.09   6.13e-4       S
+#PacBio-WT                5926            73204               7.49   0.0158 1e+0          NS
 
 #Grouped by amount of PCR:  ----
 
@@ -153,5 +155,8 @@ print(results_grouped)
 #Protocol_Group           Delta10q_Reads FullLength_Reads Delta10q_Percentage Log2FC Fisher_p
 #PCR_cDNA_only                      8931            97170                8.42  0.184 6.01e- 1
 #PCR_cDNA_plus_enrichment          33244            74974               30.7   2.05  1.79e-22
+
+
+
 
 
